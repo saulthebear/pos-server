61ffb43553addb9168c0d6ed513b03eef9f94ee3
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-24 11:12:42 -0600

    Update User model and controller
    Co-authored-by: gracenarez333 <gracenarez333@users.noreply.github.com>
    Co-authored-by: Heg <erhaneth@users.noreply.github.com>

--- controllers/api/v1/users.js
+++ controllers/api/v1/users.js
@@ -4,18 +4,29 @@ const bcrypt = require("bcrypt")
 const jwt = require("jsonwebtoken")
 const { requireUser } = require("../../../middleware/authMiddleware")
 
+const createJWT = (user) => {
+  const payload = {
+    username: user.username,
+    id: user.id,
+    role: user.role,
+  }
+  // sign the token and send it back
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" }) // expires in one day
+  return token
+}
+
 // POST /users/register -- CREATE a new user
 router.post("/register", async (req, res) => {
   try {
     // check if the user exists already
     const findUser = await db.User.findOne({
-      email: req.body.email,
+      username: req.body.username,
     })
 
     // disallow users from registering twice
     if (findUser) {
       // stop the route and send a response saying the user exists
-      return res.status(400).json({ msg: "email exists already 🤦‍♂️" })
+      return res.status(400).json({ msg: "username exists already 🤦‍♂️" })
     }
 
     // hash the user's password
@@ -25,30 +36,21 @@ router.post("/register", async (req, res) => {
 
     // create a new user with the hashed password
     const newUser = new db.User({
-      name: req.body.name,
-      email: req.body.email,
+      username: req.body.username,
       password: hashedPassword,
     })
     await newUser.save()
 
-    // sign the user in by sending a valid jwt back
-    // create the jwt payload
-    const payload = {
-      name: newUser.name,
-      email: newUser.email,
-      id: newUser.id,
-    }
-    // sign the token and send it back
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" }) // expires in one day
+    const token = createJWT(newUser)
     res.json({ token })
   } catch (err) {
     console.warn(err)
     // handle validation errors
     if (err.name === "ValidationError") {
-      res.status(400).json({ msg: err.message })
+      res.status(400).json({ error: err.message })
     } else {
       // handle all other errors
-      res.status(500).json({ msg: "server error 500" })
+      res.status(500).json({ error: "server error 500" })
     }
   }
 })
@@ -59,14 +61,14 @@ router.post("/login", async (req, res) => {
     // all the data will come in on the req.body
     // try to find the user in the database
     const foundUser = await db.User.findOne({
-      email: req.body.email,
+      username: req.body.username,
     })
-    const noLoginMessage = "Incorrect email or password."
+    const noLoginMessage = "Incorrect username or password."
 
     // if the user is not found, return send a status of 400 let the user know login failed
     if (!foundUser) {
-      console.log("incorrect email", req.body)
-      return res.status(400).json({ msg: noLoginMessage })
+      console.log("incorrect username", req.body)
+      return res.status(400).json({ error: noLoginMessage })
     }
 
     // check if the supplied password matches the hash in the db
@@ -77,17 +79,10 @@ router.post("/login", async (req, res) => {
     // if they do not match, return and let the user know that login has failed
     if (!passwordCheck) {
       console.log("incorrect password", req.body)
-      return res.status(400).json({ msg: noLoginMessage })
+      return res.status(400).json({ error: noLoginMessage })
     }
 
-    // create a jwt payload
-    const payload = {
-      name: foundUser.name,
-      email: foundUser.email,
-      id: foundUser.id,
-    }
-    // sign the jwt and send it back
-    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1d" })
+    const token = createJWT(foundUser)
 
     res.json({ token })
   } catch (err) {
@@ -97,7 +92,7 @@ router.post("/login", async (req, res) => {
   }
 })
 
-// GET /users/auth-locked -- checks users credentials and only send back privlaged information if the user is logged in properly
+// GET /users/auth-locked -- checks users credentials and only send back information if the user is logged in properly
 router.get("/auth-locked", requireUser, (req, res) => {
   console.log("current user is:", res.locals.user)
   res.json({ msg: "welcome to the secret auth-locked route 👋" })
--- models/user.js
+++ models/user.js
@@ -1,20 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true
-	},
-	email: {
-		type: String,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	}
-}, {
-	timestamps: true
-})
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "unverified",
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema)
