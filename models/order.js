7cf10044ae2c1a4c70da6b908426835e0e48dc8f
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-27 21:53:40 -0600

    Add refs to schemas
    Co-authored-by: gracenarez333 <gracenarez333@users.noreply.github.com>
    Co-authored-by: Heg <erhaneth@users.noreply.github.com>

--- models/order.js
+++ models/order.js
@@ -4,6 +4,7 @@ const LineItemSchema = new mongoose.Schema({
   product: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
+    ref: "Product",
   },
   price: {
     type: Number,
@@ -15,23 +16,27 @@ const LineItemSchema = new mongoose.Schema({
   },
 })
 
-const OrderSchema = new mongoose.Schema({
-  lineItems: [
-    {
-      type: LineItemSchema,
+const OrderSchema = new mongoose.Schema(
+  {
+    lineItems: [
+      {
+        type: LineItemSchema,
+        required: true,
+        validate: [validateMinLength, "Line items must have at least one item"],
+      },
+    ],
+    cashier: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
-      validate: [validateMinLength, "Line items must have at least one item"],
     },
-  ],
-  cashier: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
+    payment_method: {
+      type: String,
+      required: true,
+    },
   },
-  payment_method: {
-    type: String,
-    required: true,
-  },
-})
+  { timestamps: true }
+)
 
 function validateMinLength(val) {
   return val.length >= 1
